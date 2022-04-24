f0e570e0eb8bca79eba11ae8065a0164d3281ae1
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-04-24 14:57:16 +0200

    gg

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,7 +3,6 @@ coach1=[]
 sep=","
 archivo1="coach_1_pokemons.csv"
 with open(archivo1) as archivo:
-    next(archivo)
     for linea in archivo:
         linea=linea.rstrip("/n")
         columnas=linea.split(sep)
@@ -19,7 +18,6 @@ coach2=[]
 sep=","
 archivo2="coach_2_pokemons.csv"
 with open(archivo2) as archivo:
-    next(archivo)
     for linea in archivo:
         linea=linea.rstrip("/n")
         columnas=linea.split(sep)
@@ -31,10 +29,33 @@ with open(archivo2) as archivo:
         defensa=columnas[5]
         coach2.append(pokemon(id,nombre,arma,int(salud),int(ataque),int(defensa)))
 
+print(coach1)
+print(coach2)
 class combate_pokemon:
     def __init__(self,entrenador1,entrenador2):
         self.entrenador1=entrenador1
         self.entrenador2=entrenador2
-    def comenzar(self):
-        
+    def comenzar(self,pokemon1,pokemon2):
+        if pokemon1 not in self.entrenador1:
+            pokemon1=input(f'el pokemon utilizado no pertenece al primer entrenador, utilice un pokemon valido: ')
+        elif pokemon2 not in self.entrenador2:
+            pokemon2=input(f'el pokemon utilizado no pertenece al segundo entrenador, utilice un pokemon valido: ')
+        else:
+            while pokemon1.salud>0 and pokemon2.salud>0:
+                pokemon1.atacar(pokemon2)
+                pokemon2.atacar(pokemon1)
+            else:
+                if pokemon1.salud<=0:
+                    print(f"el pokemon {pokemon2.nombre} de {self.entrenador2} gano la primera batalla")
+                    self.entrenador1.remove(pokemon1)
+                elif pokemon2.salud<=0:
+                    print(f"el pokemon {pokemon1.nombre} de {self.entrenador1} gano la primera batalla")
+                    self.entrenador2.remove(pokemon2)
+
+
+
+
+
+combate=combate_pokemon(coach1,coach2)
+combate.comenzar(coach1[2],coach2[2])
 
