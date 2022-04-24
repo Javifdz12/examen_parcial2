69e2cfda140b9c77938ed6d1de474fdd9e90a0fb
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-04-24 15:46:18 +0200

    gg

--- ejercicio3.py
+++ ejercicio3.py
@@ -31,25 +31,28 @@ with open(archivo2) as archivo:
 
 print(coach1)
 print(coach2)
+
+class entrenador:
+    def __init__(self, list_pokemons):
+        self.list_pokemons = list_pokemons
+        self.pokemon1=list_pokemons[0]
+        self
 class combate_pokemon:
     def __init__(self,entrenador1,entrenador2):
         self.entrenador1=entrenador1
         self.entrenador2=entrenador2
-    def comenzar(self,pokemon1,pokemon2):
-        if pokemon1 not in self.entrenador1:
-            pokemon1=input(f'el pokemon utilizado no pertenece al primer entrenador, utilice un pokemon valido: ')
-        elif pokemon2 not in self.entrenador2:
-            pokemon2=input(f'el pokemon utilizado no pertenece al segundo entrenador, utilice un pokemon valido: ')
-        else:
+    def comenzar(self):
+        for pokemon1,pokemon2 in self.entrenador1,self.entrenador2:
+
             while pokemon1.salud>0 and pokemon2.salud>0:
                 pokemon1.atacar(pokemon2)
                 pokemon2.atacar(pokemon1)
             else:
                 if pokemon1.salud<=0:
-                    print(f"el pokemon {pokemon2.nombre} de {self.entrenador2} gano la primera batalla")
+                    print(f"el pokemon {pokemon2.nombre} de {self.entrenador2} gano la batalla")
                     self.entrenador1.remove(pokemon1)
                 elif pokemon2.salud<=0:
-                    print(f"el pokemon {pokemon1.nombre} de {self.entrenador1} gano la primera batalla")
+                    print(f"el pokemon {pokemon1.nombre} de {self.entrenador1} gano la batalla")
                     self.entrenador2.remove(pokemon2)
 
 
@@ -57,5 +60,5 @@ class combate_pokemon:
 
 
 combate=combate_pokemon(coach1,coach2)
-combate.comenzar(coach1[2],coach2[2])
+combate.comenzar()
 
