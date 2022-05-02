73ce8335d953784bc40b2239282a4293bff7a8e3
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-05-02 18:17:38 +0200

    gg

--- ejercicio3.py
+++ ejercicio3.py
@@ -42,13 +42,12 @@ class entrenador:
     def get_pokemon(self):
         lista1=self.list_pokemons
         i=0
-        for i in lista1[i]:
-            x=input(f"{self.nombre} debe elegir uno de sus pokemons:\n{i}- {lista1[i]}")
-            if x not in range(len(lista1)-1):
-                x=input(f"{self.nombre} debe elegir una de las opciones numericas a continuacion:\n{i}- {lista1[i]}")
-            else:
-                print(f"el entrenador eligio a {lista1[x].nombre}")
-        return x
+        print(f'{self.nombre} debe elegir uno de sus pokemons:')
+        for i in range(0,len(lista1)):
+            print(f"\n{i}- {lista1[i].nombre}")
+        x=int(input())
+        print(f'{self.nombre} eligio a {lista1[x].nombre}')
+        return int(x)
 
 
 class combate:
@@ -63,6 +62,7 @@ class combate:
             if self.pokemon1.salud<=0:
                 print(f"el pokemon {self.pokemon2.nombre} gano la batalla")
                 print(f"le quedan {self.pokemon2.salud} puntos de salud")
+
             else:
                 print(f"el pokemon {self.pokemon1.nombre} gano la batalla")
                 print(f"le quedan {self.pokemon1.salud} puntos de salud")
@@ -71,123 +71,40 @@ class combate_pokemon:
     def __init__(self,entrenador1,entrenador2):
         self.entrenador1=entrenador1
         self.entrenador2=entrenador2
-    def comenzar(self):
-            if self.entrenador1.get_pokemon()==0:
+    def combate(pokemon1,pokemon2):
+        while pokemon1.salud>0 and pokemon2.salud>0:
+            pokemon1.atacar(pokemon2)
+            pokemon2.atacar(pokemon1)
+        else:
+            if pokemon1.salud<=0:
+                print(f"el pokemon {pokemon2.nombre} gano la batalla")
+                print(f"le quedan {pokemon2.salud} puntos de salud")
 
-                if self.entrenador2.get_pokemon()==0:
-
-                    while self.entrenador1.pokemon1.salud>0 and self.entrenador2.pokemon1.salud>0:
-                        self.entrenador1.pokemon1.atacar(self.entrenador2.pokemon1)
-                        self.entrenador2.pokemon1.atacar(self.entrenador1.pokemon1)
-                    else:
-                        if self.entrenador1.pokemon1.salud<=0:
-                            self.entrenador1.list_pokemons.remove(self.entrenador1.pokemon1)
-                            print(f"el pokemon {self.entrenador2.pokemon1.nombre} de {self.entrenador2.nombre} gano la batalla")
-                            print(f"le quedan {self.entrenador2.pokemon1.salud} puntos de salud")
-                            if self.entrenador1.get_pokemon()==0:
-                                while self.entrenador1.list_pokemons[0].salud>0 and self.entrenador2.pokemon1.salud>0:
-                                    self.entrenador1.list_pokemons[0].atacar(self.entrenador2.pokemon1)
-                                    self.entrenador2.pokemon1.atacar(self.entrenador1.list_pokemons[0])
-                                else:
-                                    if self.entrenador1.list_pokemons[0].salud<=0:
-                                        self.entrenador1.list_pokemons.remove(self.entrenador1.list_pokemons[0])
-                                        print(f"el pokemon {self.entrenador2.pokemon1.nombre} de {self.entrenador2.nombre} gano la batalla")
-                                        print(f"le quedan {self.entrenador2.pokemon1.salud} puntos de salud")
-                                        print(f"A {self.entrenador1.nombre} solo le queda un pokemon")
-                                        x=self.entrenador1.get_pokemon()
-                                        while self.entrenador1.list_pokemons[x].salud>0 and self.entrenador2.pokemon1.salud>0:
-                                            self.entrenador1.pokemon1.atacar(self.entrenador2.pokemon1)
-                                            self.entrenador2.pokemon1.atacar(self.entrenador1.pokemon1)
-                                        else:
-                                            if self.entrenador1.list_pokemons[x].salud<=0:
-                                                self.entrenador1.list_pokemons.remove(self.entrenador1.list_pokemons[x])
-                                                print(f"el pokemon {self.entrenador2.pokemon1.nombre} de {self.entrenador2.nombre} gano la batalla")
-                                                print(f"le quedan {self.entrenador2.pokemon1.salud} puntos de salud")
-                                                print(f'{self.entrenador2.nombre} gano la batalla')
-                                            elif self.entrenador2.pokemon1.salud<=0:
-                                                self.entrenador2.list_pokemons.remove(self.entrenador2.pokemon2)
-                                                print(f"el pokemon {self.entrenador1.list_pokemons[0]} de {self.entrenador1.list_} gano la batalla")
-                                                print(f"le quedan {self.entrenador1.list_pokemons[0].salud} puntos de salud")
-                                                if self.entrenador2.get_pokemon()==0:
-                                                    while self.entrenador1.list_pokemons[x].salud>0 and self.entrenador2.pokemon1.salud>0:
-                                                        self.entrenador1.pokemon1.atacar(self.entrenador2.pokemon1)
-                                                        self.entrenador2.pokemon1.atacar(self.entrenador1.pokemon1)
-                                                    else:
-                                                        if self.entrenador1.list_pokemons[x].salud<=0:
-                                                            self.entrenador1.list_pokemons.remove(self.entrenador1.list_pokemons[x])
-                                                            print(f"el pokemon {self.entrenador2.pokemon1.nombre} de {self.entrenador2.nombre} gano la batalla")
-                                                            print(f"le quedan {self.entrenador2.pokemon1.salud} puntos de salud")
-                                                            print(f'{self.entrenador2.nombre} gano la batalla')
-                                                        elif self.entrenador2.pokemon1.salud<=0:
-                                                            self.entrenador2.list_pokemons.remove(self.entrenador2.pokemon2)
-                                                            print(f"el pokemon {self.entrenador1.list_pokemons[0]} de {self.entrenador1.list_} gano la batalla")
-                                                            print(f"le quedan {self.entrenador1.list_pokemons[0].salud} puntos de salud")
-                                                            print(f'A {self.entrenador2.nombre} le queda solo un pokemon')
-                                                            x=self.entrenador2.get_pokemon()
-                                                            while self.entrenador2.list_pokemons[x].salud>0 and self.entrenador1.pokemon1.salud>0:
-                                                                self.entrenador1.pokemon1.atacar(self.entrenador2.pokemon1)
-                                                                self.entrenador2.list_pokemons[x].atacar(self.entrenador1.list_pokemons[x])
-                                                            else:
-                                                                if self.entrenador2.list_pokemons[x].salud<=0:
-                                                                    self.entrenador2.list_pokemons.remove(self.entrenador1.list_pokemons[x])
-                                                                    print(f"el pokemon {self.entrenador1.pokemon1.nombre} de {self.entrenador2.nombre} gano la batalla")
-                                                                    print(f"le quedan {self.entrenador1.pokemon1.salud} puntos de salud")
-                                                                    print(f'{self.entrenador1.nombre} gano la batalla')
-                                                                elif self.entrenador2.pokemon1.salud<=0:
-                                                                    self.entrenador2.list_pokemons.remove(self.entrenador2.pokemon2)
-                                                                    print(f"el pokemon {self.entrenador1.list_pokemons[x]} de {self.entrenador1.nombre} gano la batalla")
-                                                                    print(f"le quedan {self.entrenador1.list_pokemons[x].salud} puntos de salud")
-                                                                    print(f'{self.entrenador1.nombre} le queda solo un pokemon')
-                                                else:pass
-                                    elif self.entrenador2.pokemon1.salud<=0:
-                                        pass
-                            elif self.entrenador1.get_pokemon()==1:
-                                pass
-
-                        elif self.entrenador2.pokemon1.salud<=0:
-                            self.entrenador2.list_pokemons.remove(self.entrenador2.pokemon1)
-                            print(f"el pokemon {self.entrenador1.pokemon1.nombre} de {self.entrenador1} gano la batalla")
-                            print(f"le quedan {self.entrenador1.pokemon1.salud} puntos de salud")
-                            if self.entrenador2.get_pokemon()==0:
-                                while self.entrenador2.list_pokemons[0].salud>0 and self.entrenador1.pokemon1.salud>0:
-                                    self.entrenador2.list_pokemons[0].atacar(self.entrenador2.pokemon1)
-                                    self.entrenador1.pokemon1.atacar(self.entrenador1.list_pokemons[0])
-                                else:
-                                    if self.entrenador2.list_pokemons[0].salud<=0:
-                                        self.entrenador2.list_pokemons.remove(self.entrenador2.list_pokemons[0])
-                                        print(f"el pokemon {self.entrenador1.pokemon1.nombre} de {self.entrenador2.nombre} gano la batalla")
-                                        print(f"le quedan {self.entrenador1.pokemon1.salud} puntos de salud")
-                                        print(f"A {self.entrenador2.nombre} solo le queda un pokemon")
-                                        x=self.entrenador2.get_pokemon()
-                                        while self.entrenador1.list_pokemons[x].salud>0 and self.entrenador2.pokemon1.salud>0:
-                                            self.entrenador1.pokemon1.atacar(self.entrenador2.pokemon1)
-                                            self.entrenador2.pokemon1.atacar(self.entrenador1.pokemon1)
-                                        else:
-                                            if self.entrenador2.pokemon1.salud<=0:
-                                                self.entrenador2.list_pokemons.remove(self.entrenador2.list_pokemons[x])
-                                                print(f"el pokemon {self.entrenador1.pokemon1.nombre} de {self.entrenador1.nombre} gano la batalla")
-                                                print(f"le quedan {self.entrenador2.pokemon1.salud} puntos de salud")
-                                                print(f'{self.entrenador2.nombre} gano el combate!!!')
-                                            elif self.entrenador2.pokemon1.salud<=0:
-                                                self.entrenador2.list_pokemons.remove(self.entrenador2.pokemon2)
-                                                print(f"el pokemon {self.entrenador1.list_pokemons[0]} de {self.entrenador1.list_} gano la batalla")
-                                                print(f"le quedan {self.entrenador1.list_pokemons[0].salud} puntos de salud")
-
-                elif self.entrenador2.get_pokemon()==1:
-                    pass
-                else:
-                    pass
-            elif self.entrenador1.get_pokemon()==1:
-                pass
             else:
-                pass
+                print(f"el pokemon {pokemon1.nombre} gano la batalla")
+                print(f"le quedan {pokemon1.salud} puntos de salud")
 
-
-
-
-
-
-
+    def comenzar(self):
+        a=self.entrenador1.get_pokemon()
+        b=self.entrenador2.get_pokemon()
+        print("la batalla comienza en 3....,2....,1....")
+        while self.entrenador1.list_pokemons!=[] and self.entrenador2.list_pokemons!=[]:
+            combate1=combate_pokemon.combate(self.entrenador1.list_pokemons[a],self.entrenador2.list_pokemons[b])
+            if self.entrenador1.list_pokemons[a].salud<=0:
+                self.entrenador1.list_pokemons.remove(self.entrenador1.list_pokemons[a])
+                if self.entrenador1.list_pokemons!=[]:
+                    a=self.entrenador1.get_pokemon()
+            else:
+                self.entrenador2.list_pokemons.remove(self.entrenador2.list_pokemons[b])
+                if self.entrenador2.list_pokemons!=[]:
+                    b=self.entrenador2.get_pokemon()
+        else:
+            if self.entrenador1.list_pokemons==[]:
+                print(f"{self.entrenador2.nombre} gano el combate")
+                print("!!ENHORABUENA¡¡")
+            else:
+                print(f"{self.entrenador1.nombre} gano el combate")
+                print("!!ENHORABUENA¡¡")
 
 
 combate=combate_pokemon(entrenador(coach1,"javi"),entrenador(coach2,"ruben"))
